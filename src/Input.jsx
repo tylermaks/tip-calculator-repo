b1f01c9c904fc2d1b55e7d74406b39d5b9cded6c
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-13 17:47:42 -0400

    Add desktop css

--- src/Input.jsx
+++ src/Input.jsx
@@ -4,15 +4,21 @@ import Percentages from "./Percentages"
 
 function Input(){
     return(
-        <section className="input-section">
+        <div className="input-section">
             <div className="input-container">
-                <label htmlFor="bill">Bill</label>
-                <input id="bill" type="text" />
+                <div>
+                    <label htmlFor="bill">Bill</label>
+                    <input id="bill" type="text" />
+                </div>
+                
                 <Percentages />
-                <label htmlFor="people">Number of People</label>
-                <input type="text" name="people"  />
+
+                <div>
+                    <label htmlFor="people">Number of People</label>
+                    <input type="text" name="people"  />
+                </div>
             </div>
-        </section>
+        </div>
     )
 }
 
--- src/Output.jsx
+++ src/Output.jsx
@@ -7,24 +7,26 @@ function Output() {
     const tipOutput = ["Tip Amount", "Total"]
 
     return(
-        <section className="output-section">
-            {
-                tipOutput.map(item => {
-                    return(
-                        <div className="tip-output">
-                        <div>
-                            <span className="amount">{item}</span>
-                            <span className="per-person">/  person</span>
-                        </div>
-                        <div>
-                            <h2>$0.00</h2>
-                        </div>
-                    </div>
-                    )
-                })
-            } 
+        <div className="output-section">
+            <div>
+                {
+                    tipOutput.map(item => {
+                        return(
+                            <div className="tip-output">
+                                <div>
+                                    <span className="amount">{item}</span>
+                                    <span className="per-person">/  person</span>
+                                </div>
+                                <div>
+                                    <h2>$0.00</h2>
+                                </div>
+                            </div>
+                        )
+                    })
+                } 
+            </div>
             <button className="reset-btn">Reset</button>
-        </section>
+        </div>
     )
 }
 
