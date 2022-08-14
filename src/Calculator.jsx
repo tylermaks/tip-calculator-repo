213617a621d589522bc537712c3531879454e7a9
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-14 11:44:13 -0400

    Add input validation and state

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import Input from "./Input"
 import Output from "./Output"
 
 import './Assets/Styles/Calculator/Calculator.css'
 
 function Calculator(){
+
+    const [billAmount, setBillAmount] = useState(0)
+
+    const handleChange = (e) =>{
+        setBillAmount(e.target.value)
+    }
+
     return(
         <section className="calc-container">
-            <Input />
+            <Input 
+                onFocusOut={handleChange}
+                billAmount={billAmount}
+            />
             <Output />
         </section>
     )
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,20 +2,37 @@ import React from "react"
 
 import Percentages from "./Percentages"
 
-function Input(){
+function Input(props){
+
+    const checkError =  props.billAmount > 0 ? "Must be a number" : null
+
     return(
         <div className="input-section">
             <div className="input-container">
                 <div>
                     <label htmlFor="bill">Bill</label>
-                    <input id="bill" type="text" />
+                    <input 
+                        id="bill" 
+                        type="number" 
+                        name="bill"
+                        pattern="^[0-9]*$"
+                        placeholder="0"
+                        onFocusOut={props.onFocusOut}
+                    />
                 </div>
                 
                 <Percentages />
 
                 <div>
                     <label htmlFor="people">Number of People</label>
-                    <input type="text" name="people"  />
+                    <span className="error">{checkError}</span>
+                    <input 
+                        id="people"
+                        type="number" 
+                        name="people"  
+                        pattern="^[0-9]*$"
+                        placeholder="0"
+                    />
                 </div>
             </div>
         </div>
